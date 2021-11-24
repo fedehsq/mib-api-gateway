780e668fc49f10ebd64732298cbbb46f16eba4ac
nicolo <nicolopierami@gmail.com>
2021-11-24 15:56:59 +0100

    ms message added

--- mib/views/__init__.py
+++ mib/views/__init__.py
@@ -1,7 +1,8 @@
 from .auth import auth
 from .home import home
 from .users import users
+from .mailbox import mailbox
 
 """List of the views to be visible through the project
 """
-blueprints = [home, auth, users]
+blueprints = [home, auth, users, mailbox]
